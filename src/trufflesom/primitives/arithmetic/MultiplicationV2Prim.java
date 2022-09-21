0a9ad2e0c6cf3100b0254e82f2fb9620d40e6a4b
Octave Larose <octave.larose@hotmail.fr>
2022-09-21 10:26:20 +0100

    argumentreadv2node executeLong replacing+inlining

---
+++ src/trufflesom/primitives/arithmetic/MultiplicationV2Prim.java
@@ -0,0 +1,80 @@
+package trufflesom.primitives.arithmetic;
+
+import bdt.primitives.Primitive;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.GenerateNodeFactory;
+import com.oracle.truffle.api.dsl.Specialization;
+import trufflesom.vm.SymbolTable;
+import trufflesom.vmobjects.SSymbol;
+
+import java.math.BigInteger;
+
+
+@GenerateNodeFactory
+//@Primitive(className = "Integer", primitive = "*")
+//@Primitive(className = "Double", primitive = "*")
+//@Primitive(selector = "*")
+public abstract class MultiplicationV2Prim extends ArithmeticPrim {
+  @Override
+  public SSymbol getSelector() {
+    return SymbolTable.symbolFor("*");
+  }
+
+  @Specialization(rewriteOn = ArithmeticException.class)
+  public final long doLong(final long left, final long right) {
+    return Math.multiplyExact(left, right);
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public final Object doLongWithOverflow(final long left, final long right) {
+    return reduceToLongIfPossible(
+        BigInteger.valueOf(left).multiply(BigInteger.valueOf(right)));
+  }
+
+  @Specialization
+  public final double doLong(final long left, final double right) {
+    return doDouble(left, right);
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public final Object doLong(final long left, final BigInteger right) {
+    return doBigInteger(BigInteger.valueOf(left), right);
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public final Object doBigInteger(final BigInteger left, final long right) {
+    return doBigInteger(left, BigInteger.valueOf(right));
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public final Object doDouble(final BigInteger left, final double right) {
+    return left.doubleValue() * right;
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public final Object doBigInteger(final BigInteger left, final BigInteger right) {
+    BigInteger result = left.multiply(right);
+    return reduceToLongIfPossible(result);
+  }
+
+  @Specialization
+  public final double doDouble(final double left, final double right) {
+    return left * right;
+  }
+
+  @Specialization
+  public final double doDouble(final double left, final long right) {
+    return left * right;
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public final Object doDouble(final double left, final BigInteger right) {
+    return left * right.doubleValue();
+  }
+}
