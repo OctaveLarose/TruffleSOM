4cffa3eedb641a93408ebdce871f96f7e9a28fa4
Octave Larose <octave.larose@hotmail.fr>
2022-09-27 16:48:54 +0100

    temporarily deactivating the double method, leaving just the long one

--- src/trufflesom/interpreter/nodes/ArgumentReadV2Node.java
+++ src/trufflesom/interpreter/nodes/ArgumentReadV2Node.java
@@ -25,7 +25,7 @@ public abstract class ArgumentReadV2Node {
       if (arg.index > 10000) {
         // if these methods are never called, I can't use them to replace existing calls... bad hack
         this.doLong(new FrameWithoutBoxing(new FrameDescriptor(), new Object[]{Long.valueOf(42), Long.valueOf(42)}));
-        this.doDouble(new FrameWithoutBoxing(new FrameDescriptor(), new Object[]{42.0, 42.0}));
+//        this.doDouble(new FrameWithoutBoxing(new FrameDescriptor(), new Object[]{42.0, 42.0}));
       }
     }
 
@@ -46,9 +46,9 @@ public abstract class ArgumentReadV2Node {
       return (long) frame.getArguments()[argumentIndex];
     }
 
-    public final double doDouble(final VirtualFrame frame) {
-      return (double) frame.getArguments()[argumentIndex];
-    }
+//    public final double doDouble(final VirtualFrame frame) {
+//      return (double) frame.getArguments()[argumentIndex];
+//    }
 
     @Override
     public void replaceAfterScopeChange(final ScopeAdaptationVisitor inliner) {
