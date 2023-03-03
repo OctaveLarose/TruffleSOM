9ee10867a6a026caa25c1e4b0829d1dfaa1f86bc
Octave Larose <octave.larose@hotmail.fr>
2023-03-03 11:48:17 +0000

    no more lowered controls structs

--- src/trufflesom/primitives/Primitives.java
+++ src/trufflesom/primitives/Primitives.java
@@ -133,7 +133,7 @@ public final class Primitives extends PrimitiveLoader<ExpressionNode, SSymbol> {
 
   static {
     specializer = initSpecializers();
-    inlinableNodes = new InlinableNodes<>(SymbolTable.SymbolProvider, null, getInlinableFactories());
+    inlinableNodes = new InlinableNodes<>(SymbolTable.SymbolProvider, null, null);
     Current = new Primitives();
   }
 
