3fa473c176c905eb85a15d828f5d50c3627f32bf
Octave Larose <octave.larose@hotmail.fr>
2023-04-05 16:25:15 +0100

    fixed test bench on bc no inline caching

--- src/trufflesom/primitives/basics/EqualsEqualsPrim.java
+++ src/trufflesom/primitives/basics/EqualsEqualsPrim.java
@@ -7,6 +7,8 @@ import com.oracle.truffle.api.dsl.Specialization;
 import bdt.primitives.Primitive;
 import trufflesom.interpreter.nodes.nary.BinaryExpressionNode;
 
+import java.math.BigInteger;
+
 
 @GenerateNodeFactory
 @Primitive(className = "Integer", primitive = "==")
@@ -18,6 +20,11 @@ public abstract class EqualsEqualsPrim extends BinaryExpressionNode {
   @Specialization
   public static final boolean doLong(final long left, final long right) {
     return left == right;
+  }
+
+  @Specialization
+  public static final boolean doBigInt(final BigInteger left, final BigInteger right) {
+    return left.equals(right);
   }
 
   @Specialization
